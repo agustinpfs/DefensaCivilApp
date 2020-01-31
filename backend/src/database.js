101b51e68cc2a9281563bdd825991943ec0cbc92
Agustin Palmieri <agujex@Agustins-MBP.utopia.net>
2020-01-31 12:24:30 -0300

    update database.js

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,21 +1,70 @@
-const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config();  //before the application starts the variable starts .env
 
 
 
-const URI = process.env.MONGOOSE_URI
-    ? process.env.MONGOOSE_URI
-    : 'mongodb://localhost/dcivildatabase';
+// var db
+
+MongoClient.connect(
+    process.env.MONGODB_URI,
+    (err, client) => {
+        if (err) return console.log(err)
+        db = client.db(process.env.MONGO_NS) // whatever your database name is
+    })
 
 
-mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-});
+// process.env.MONGODB_URI,
+//   { useNewUrlParser: true },
 
 
-const connection = mongoose.connection;
 
-connection.once('open', () => {
-    console.log('Database is connected');
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+
+
+// const URI = process.env.MONGOOSE_URI
+//     ? process.env.MONGOOSE_URI
+//     : 'mongodb://localhost/dcivildatabase';
+
+
+// mongoose.connect(URI, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+// });
+
+
+// const connection = mongoose.connection;
+
+// connection.once('open', () => {
+//     console.log('Database is connected');
+// });
