8fa6511d27261b3171c55a3b10569f304c225649
Agustin Palmieri <agujex@Agustins-MBP.utopia.net>
2020-01-31 12:51:52 -0300

    add new shema

--- backend/src/models/Geo.js
+++ backend/src/models/Geo.js
@@ -1,4 +1,3 @@
-// const { Schema, model } = require('mongoose');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -8,21 +7,22 @@ const Schema = mongoose.Schema;
 
 const geoSchema = new Schema({
   description: String,
-  features: [{}]
+  type: String,
+  // Array of subdocuments
+  features: 
+          [{
+              type: String,
+              properties: {
+                          desc = Object,
+                          image = Object
+                          },
+              geometry: {
+                          type: String,
+                          coordinates: [Number]
+              }
+              
+          }],
 })
-
-
-
-// [{
-//   "features": [
-//     {
-//       "type": "Feature",
-//       "properties": { "desc": null, "image": null },
-//       "geometry": { "type": "Point", "coordinates": [-60.94809293746948, -34.586230922487545] }
-//     }],
-//     "_id": "5e2f4b77cd2773512d422ed1", "__v": 0
-// }]
-
 
 
 
