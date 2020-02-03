ad788cbb5fdc9ea7f00cacc1f93b693cd198b99a
Agustin Palmieri <agujex@Agustins-MBP.utopia.net>
2020-02-03 05:35:29 -0300

    update schema

--- backend/src/models/Geo.js
+++ backend/src/models/Geo.js
@@ -1,6 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const geoSchema = new Schema({
+  description: { $type: String },
+  type: { $type: String },  
+
+  // Array of subdocuments
+  features: [{
+    type: String,
+    properties: {
+      desc : Object,
+      image : Object
+      },
+    geometry: {
+      type: { $type: String },
+      coordinates: [Number]
+    }
+  }],
+  // Single subdocument
+ 
+},{typeKey: '$type'})
+
+
 
 
 
